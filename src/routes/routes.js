83ddbc40e9061e16ab571c8441b9015b3cdfd7a9
mubashir <mubashir22ali@gmail.com>
2017-09-17 21:55:45 +0500

    debug

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,16 +3,18 @@ import { UserAuth } from '../controller/controller'
 
 export const router = Router();
 
-
+router.get('/check',(req,res)=>{
+res.send('checking')
+})
 router.get('/',(req,res)=>{
-    res.send("kaka")
+    res.send("kakaaa")
 })
 
 // router.post('/register', UserAuth.register);
 
 // router.post('/login',UserAuth.login);
 
-router.post('/patient',UserAuth.storeallusers);
+router.get('/patient',UserAuth.storeallusers);
 
 router.get('/allPatient',UserAuth.getAllPatient);
 
